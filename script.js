0aad7cb942f443a6bd330c159ee171c60c3200d9
emmadumbi <dumbiemmanuel29@gmail.com>
2025-02-26 23:48:16 +0100

    added some part of the form validation

--- script.js
+++ script.js
@@ -1,45 +1,104 @@
-'use strict'
-const form = document.querySelector('#form');
-const firstName = document.querySelector('#firstName');
-const lastName = document.querySelector('#lastName');
-const email = document.querySelector('#email');
-const textArea = document.querySelector('#textArea');
-// const term = document.querySelector('#term');
-const submitBtn = document.querySelector('#submitBtn');
+"use strict";
+const firstName = document.querySelector("#firstName");
+const lastName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const textArea = document.querySelector("#textArea");
+const term = document.querySelector("#term");
+const termError = document.querySelector("#termError");
+const userChoice = document.getElementsByName("userChoice");
+const errorText = document.querySelector("#error");
+const submitBtn = document.querySelector("#submitBtn");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    validateInputs();
+  validateInputs();
 });
 
-const showError = (input, message) => {
-    const errorEl = input.nextElementSibling;
-    errorEl.textContent = message;
-};
-
-const showSuccess = (input) => {
-    const errorEl = input.nextElementSibling;
-    errorEl.textContent = '';
+function showError(input, message) {
+  const errorEl = input.nextElementSibling;
+  errorEl.textContent = message;
+  errorEl.style.color = "hsl(0, 66%, 54%)";
+  errorEl.style.fontWeight = "bold";
+  errorEl.style.fontSize = "14px";
+  input.style.borderColor = "hsl(0, 66%, 54%)";
 }
 
+function showSuccess(input) {
+  const errorEl = input.nextElementSibling;
+  errorEl.textContent = "";
+}
 
-const validateInputs = () => {
-    const firstNameValue = firstName.value.trim();
-    const lastNameValue = lastName.value.trim();
-    const emailValue = email.value.trim();
-    const textAreaValue = textArea.value.trim();
-    // const term = term.value.trim();
+function isValid(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
 
-    if (firstNameValue === '') {
-        showError(firstName, 'This field is required');
-    } else {
-        showSuccess(firstName);
-    };
+function validateInputs() {
+  const firstNameValue = firstName.value.trim();
 
-    if (lastNameValue === '') {
-        showError(lastName, 'This field is required');
-    } else {
-        showSuccess(lastName)
+  if (firstNameValue === "") {
+    showError(firstName, "This field is required");
+  } else {
+    showSuccess(firstName);
+  }
+
+  const lastNameValue = lastName.value.trim();
+
+  if (lastNameValue === "") {
+    showError(lastName, "This field is required");
+  } else {
+    showSuccess(lastName);
+  }
+
+  const emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    showError(email, "This field is required");
+  } else if (!isValid(emailValue)) {
+    showError(email, "Please enter a valid email address");
+  } else {
+    showSuccess(email);
+  }
+
+  let isSelected = false;
+
+  for (let i = 0; i < userChoice.length; i++) {
+    if (userChoice[i].checked) {
+      isSelected = true;
+      break;
     }
+  }
+
+  if (!isSelected) {
+    errorText.textContent = "Please select a query type";
+    errorText.style.color = "hsl(0, 66%, 54%)";
+    errorText.style.fontSize = "14px";
+    errorText.style.fontWeight = "bold";
+  } else {
+    errorText.textContent = "";
+  }
+
+  const messageEl = textArea.value.trim();
+
+  if (messageEl === "") {
+    showError(textArea, "This field is required");
+  } else {
+    showSuccess(textArea);
+  }
+
+  let isChecked = false;
+
+  if (term.checked) {
+    isChecked = true;
+  }
+
+  if (!isChecked) {
+    termError.textContent =
+      "To submit this form, please consent to being contacted";
+    termError.style.color = "hsl(0, 66%, 54%)";
+    termError.style.fontSize = "14px";
+    termError.style.fontWeight = "bold";
+  } else {
+    termError.textContent = "";
+  }
 }
